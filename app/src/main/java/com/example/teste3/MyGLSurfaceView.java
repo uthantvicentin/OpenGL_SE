9b223927515d46cb1705cc2b26c3d59d320129de
Uthant Vicentin <uthantvicentin@gmail.com>
2020-10-31 18:33:12 -0400

    DALE!

--- app/src/main/java/com/example/teste3/MyGLSurfaceView.java
+++ app/src/main/java/com/example/teste3/MyGLSurfaceView.java
@@ -1,7 +1,11 @@
 package com.example.teste3;
 
 import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import javax.microedition.khronos.opengles.GL10;
 
 class MyGLSurfaceView extends GLSurfaceView {
 
