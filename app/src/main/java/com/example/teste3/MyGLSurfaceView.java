87af1f940c451cd0ee498e1cdc8943cafab32769
Uthant Vicentin <uthantvicentin@gmail.com>
2020-10-31 17:31:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/teste3/MyGLSurfaceView.java
@@ -0,0 +1,21 @@
+package com.example.teste3;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer renderer;
+
+    public MyGLSurfaceView(Context context){
+        super(context);
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        renderer = new MyGLRenderer();
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(renderer);
+    }
+}
